013c249c2b346ee2a643ab6a2136d7fdb562ae31
Carlos Arenas <theprocess.off@gmail.com>
2021-10-13 00:24:52 -0500

    depresive commit

--- binary_search.py
+++ binary_search.py
@@ -1,24 +1,29 @@
 import random;
 
-list = [];
+def binaryStupidSearch(length, magicNumber):
 
-for i in range(0,100):
-    list.append(random.randint(10,101));
+    list = [];
 
-list.sort();
+    for i in range(0,length):
+        list.append(random.randint(10,length));
 
-print(list);
-middle = int(len(list) / 2);
-a = list[middle]
-for i in list:
-    
-    if(list[middle] <= 33):
-        middle = middle + 1;
-        break
+    list.sort();
+
+    print(list);
+    middle = int(len(list) / 2);
+    a = list[middle]
+    for i in list:
+        
+        if(list[middle] <= magicNumber):
+            middle = middle + 1;
+            break
+        else:
+            middle = middle - 1;
+
+    if(list[middle - 1] != magicNumber):
+        print('Not found, 404');
     else:
-        middle = middle - 1;
+        print('Found number:', list[middle - 1] , 'at index:' , middle - 1 );
 
-if(list[middle - 1] != 33):
-    print('Not found, 404');
-else:
-    print('Found number:', list[middle - 1] , 'at index:' , middle - 1 );
+
+binaryStupidSearch(100000, 333)
