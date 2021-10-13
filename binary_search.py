a4afdc2b0c84ebcac9557a20fc3e613707b1b784
Carlos Arenas <theprocess.off@gmail.com>
2021-10-13 18:46:32 -0500

    support for input data

--- binary_search.py
+++ binary_search.py
@@ -5,24 +5,29 @@ def binaryStupidSearch(length, magicNumber):
     list = [];
 
     for i in range(0,length):
-        list.append(random.randint(10,length));
+        list.append(random.randint(1,length));
 
     list.sort();
-
-    print(list);
+    print('List of random and sorted Numbers:',list);
     middle = int(len(list) / 2);
     for i in list:
-        
-        if(list[middle] <= magicNumber):
+        if(list[middle] == magicNumber):
+            a = list[middle];
+            break;
+        if(list[middle] < magicNumber):
             middle = middle + 1;
-            break
+            a = list[middle];
         else:
             middle = middle - 1;
+            a = list[middle];
 
-    if(list[middle - 1] != magicNumber):
+    if(a != magicNumber):
         print('Not found, 404');
     else:
-        print('Found number:', list[middle - 1] , 'at index:' , middle - 1 );
+        print('Found number:', magicNumber , 'at index:' , middle + 1);
+ 
 
+listSize = input("Enter the length of the list: ");
+criteria = input("Enter a number to find: ");
 
-binaryStupidSearch(100000, 333)
+binaryStupidSearch(int(listSize), int(criteria));
