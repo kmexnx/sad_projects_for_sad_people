238a09cb72c76aaa57855ba6b47dea83ea962877
Carlos Arenas <theprocess.off@gmail.com>
2022-01-23 23:20:32 -0600

    test complex

---
+++ linear algebra/complex_numbers_1.py
@@ -0,0 +1,20 @@
+import cmath;
+
+z1 = 4+3j;
+print(z1);
+
+print(z1.conjugate());
+
+z2 = complex(1,7);
+print(type(z2));
+
+# get imaginary of z2
+
+print(z2.imag);
+
+#value of xcomplex N does not exist 
+
+#4^2 - 3^2 = 16 - 9 = 7
+# sqrt(4^2 + 3^2)  = sqrt(25) = 5 
+print(abs(z1));
+
